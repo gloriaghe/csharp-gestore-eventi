0796987c6908cebccc677c744e1491260dd1723f
gloriaghe <gloghe@yahoo.it>
2022-11-09 14:46:06 +0100

    Aggiungere i file di progetto.

---
+++ Evento.cs
@@ -0,0 +1,70 @@
+﻿public class Evento
+{
+    private string _titolo;
+    private DateOnly _data;
+    private int _postiMassimiCapienza;
+    private int _postiPrenotati;
+
+    public string Titolo
+    {
+        get
+        {
+            return _titolo;
+        }
+        set
+        {
+            if (value == null || value == "")
+                throw new GestoreEventiException("Il nome non può essere vuoto");
+            _titolo = value;
+        }
+    }
+    public DateOnly Data
+    {
+        get
+        {
+            return _data;
+        }
+        set
+        {
+            DateOnly oggi = DateOnly.FromDateTime(DateTime.Now);
+            if (value == null || value < oggi)
+                throw new GestoreEventiException("La data non può essere precendete ad oggi");
+            _data = value;
+        }
+    }
+    public int PostiMassimiCapienza
+    {
+        get
+        {
+            return _postiMassimiCapienza;
+        }
+        private set
+        {
+            if (value == null || value <= 0)
+                throw new GestoreEventiException("La capienza deve essere maggiore di 0");
+            _postiMassimiCapienza = value;
+        }
+        
+    }
+    public int PostiPrenotati
+    {
+        get
+        {
+            return _postiPrenotati;
+        }
+        private set
+        {
+            _postiPrenotati = value;
+
+        }
+    }
+
+    public Evento(string titolo, DateOnly data, int postiMassimiCapienza)
+    {
+        Titolo = titolo;
+        Data = data;
+        PostiMassimiCapienza = postiMassimiCapienza;
+        PostiPrenotati = 0;
+    }
+
+}
---
+++ GestoreEventiException.cs
@@ -0,0 +1,21 @@
+﻿using System.Runtime.Serialization;
+
+[Serializable]
+public class GestoreEventiException : Exception
+{
+    public GestoreEventiException()
+    {
+    }
+
+    public GestoreEventiException(string? message) : base(message)
+    {
+    }
+
+    public GestoreEventiException(string? message, Exception? innerException) : base(message, innerException)
+    {
+    }
+
+    protected GestoreEventiException(SerializationInfo info, StreamingContext context) : base(info, context)
+    {
+    }
+}
