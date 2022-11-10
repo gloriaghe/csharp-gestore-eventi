95d7aa465c943c990c640c5e61315748b5d4596e
gloriaghe <gloghe@yahoo.it>
2022-11-10 11:46:49 +0100

    sistemata gestione errori seconda parte

--- Program.cs
+++ Program.cs
@@ -108,68 +108,95 @@ if (sceltaUserMenù == 1)
 }
 else if (sceltaUserMenù == 2)
 {
-    Console.Write("Vuoi creare un nuovo Evento? Digita SI o NO: ");
+    //Console.Write("Vuoi creare un nuovo Evento? Digita SI o NO: ");
+    //string sceltaUser = Console.ReadLine();
+    Evento evento = null;
+    int postiPrenotati = 0;
+
+
+    //if (sceltaUser == "SI")
+    //{
+    bool eventoOK = true;
+    while (eventoOK)
+    {
+        bool continua = false;
+        try
+        {
+            Console.WriteLine();
+            Console.Write("Inserisci il titolo dell'Evento: ");
+            string titolo = Console.ReadLine();
+
+            DateOnly dataonly = new DateOnly();
+            int postiMassimi = 0;
+
+
+            while (!continua)
+            {
+                try
+                {
+                    Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
+                    string data = Console.ReadLine();
+                    dataonly = DateOnly.Parse(data);
+                    continua = true;
+                }
+                catch (FormatException)
+                {
+                    Console.WriteLine("error: devi inserire un valore adatto ad una data ");
+                }
+            }
+            //while (!continua)
+            //{
+            //    try
+            //    {
+            Console.Write("Inserisci i posti totali dell'Evento: ");
+            postiMassimi = Convert.ToInt32(Console.ReadLine());
+            //continua = true;
+            //    }
+            //    catch (FormatException)
+            //    {
+            //        Console.WriteLine("error: devi inserire un numero");
+            //    }
+            //    catch (Exception)
+            //    {
+            //        Console.WriteLine("Errore di inserimento");
+            //    }
+            //}
+            evento = new Evento(titolo, dataonly, postiMassimi);
+            eventoOK = false;
+
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("error: devi inserire un numero");
+        }
+        catch (GestoreEventiException)
+        {
+            Console.WriteLine("Le informazioni inserite non sono corrette ricrea l'evento");
+        }
+        catch (Exception)
+        {
+            Console.WriteLine("C'è stao un'errore... ricrea l'evento");
+
+        }
+    }
+    Console.Write("Evento creato. Vuoi prenotare dei posti? (SI o NO) ");
     string sceltaUser = Console.ReadLine();
+
+
     if (sceltaUser == "SI")
     {
-        Console.WriteLine();
-        Console.Write("Inserisci il titolo dell'Evento: ");
-        string titolo = Console.ReadLine();
-
-        bool continua = true;
-        DateOnly dataonly = new DateOnly();
-        int postiMassimi = 0;
-        int postiPrenotati = 0;
-
-        while (continua)
-        {
-            try
-            {
-                Console.Write("Inserisci la data dell'evento (gg/mm/yyyy): ");
-                string data = Console.ReadLine();
-                dataonly = DateOnly.Parse(data);
-                continua = false;
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("error: devi inserire un valore adatto ad una data ");
-            }
-        }
-        while (!continua)
-        {
-            try
-            {
-                Console.Write("Inserisci i posti totali dell'Evento: ");
-                postiMassimi = Convert.ToInt32(Console.ReadLine());
-                continua = true;
-            }
-            catch (FormatException)
-            {
-                Console.WriteLine("error: devi inserire un numero");
-            }
-            catch (Exception)
-            {
-                Console.WriteLine("Errore di inserimento");
-            }
-        }
-        Evento evento = new Evento(titolo, dataonly, postiMassimi);
-
-
-        Console.Write("Evento creato. Vuoi prenotare dei posti? (SI o NO)   ");
-        sceltaUser = Console.ReadLine();
-
-
-        if (sceltaUser == "SI")
+        bool disdiciOK = false;
+        while (!disdiciOK)
         {
             Console.Write("Quanti posti vuoi prenotare? ");
-
-            while (continua)
+            bool PrenotazioneOK = false;
+            while (!PrenotazioneOK)
             {
                 try
                 {
                     postiPrenotati = Convert.ToInt32(Console.ReadLine());
 
-                    continua = false;
+                    PrenotazioneOK = true;
                 }
                 catch (FormatException)
                 {
@@ -180,41 +207,75 @@ else if (sceltaUserMenù == 2)
                     Console.WriteLine("Errore di inserimento");
                 }
             }
-            evento.PrenotaPosti(postiPrenotati);
-            Console.WriteLine("Numero di posti disponibili: " + (evento.PostiMassimiCapienza - evento.PostiPrenotati));
-            Console.WriteLine("Numero di posti prenotati: " + evento.PostiPrenotati);
 
-        }
-        else
-        {
-            Console.WriteLine("Ok. Nessun posto prenotato!");
-
-        }
-        bool postiDisdetti = true;
-        while (postiDisdetti)
-        {
-            Console.Write("Vuoi disdire dei posti? SI/NO ");
-            sceltaUser = Console.ReadLine();
-            if (sceltaUser == "SI")
+            try
             {
-                Console.Write("Quanti posti vuoi disdire? ");
-                int numDisdetti = Convert.ToInt32(Console.ReadLine());
-                evento.DisdiciPosti(numDisdetti);
-                Console.WriteLine("Numero di posti disponibili: " + (evento.PostiMassimiCapienza - evento.PostiPrenotati));
-                Console.WriteLine("Numero di posti prenotati: " + evento.PostiPrenotati);
-            }
-            else
-            {
-                Console.Write("Nessun posto disdetto!");
-                postiDisdetti = false;
+                evento.PrenotaPosti(postiPrenotati);
+                disdiciOK = true;
 
             }
-        }
+            catch (GestoreEventiException)
+            {
+                Console.WriteLine("Errore! I posti prenotati sono maggiori dei posti disponibili");
+            }
 
+        }
+        Console.WriteLine("Numero di posti disponibili: " + (evento.PostiMassimiCapienza - evento.PostiPrenotati));
+        Console.WriteLine("Numero di posti prenotati: " + evento.PostiPrenotati);
 
     }
     else
     {
-        Console.WriteLine("A presto!");
+        Console.WriteLine("Ok. Nessun posto prenotato!");
+
     }
+    bool postiDisdetti = true;
+    while (postiDisdetti)
+    {
+        Console.Write("Vuoi disdire dei posti? SI/NO ");
+        sceltaUser = Console.ReadLine();
+        if (sceltaUser == "SI")
+        {
+            bool disdiciOK = false;
+            while (!disdiciOK)
+            {
+                Console.Write("Quanti posti vuoi disdire? ");
+                int numDisdetti = Convert.ToInt32(Console.ReadLine());
+
+                try
+                {
+                    evento.DisdiciPosti(numDisdetti);
+                    disdiciOK = true;
+
+                }
+                catch (GestoreEventiException)
+                {
+                    Console.WriteLine("Non ci sono posti da disdire a sufficenza. I posti prenotati rimasti sono: " + evento.PostiPrenotati);
+                }
+
+            }
+            Console.WriteLine("Numero di posti disponibili: " + (evento.PostiMassimiCapienza - evento.PostiPrenotati));
+            Console.WriteLine("Numero di posti prenotati: " + evento.PostiPrenotati);
+            if(evento.PostiPrenotati == 0)
+            {
+                Console.WriteLine("Hai disdetto tutti i posti!");
+                postiDisdetti = false;
+
+            }
+        }
+        else
+        {
+            postiDisdetti = false;
+
+            Console.Write("Nessun posto disdetto!");
+
+        }
+    }
+
+
+    //}
+    //else
+    //{
+    //    Console.WriteLine("A presto!");
+    //}
 }
